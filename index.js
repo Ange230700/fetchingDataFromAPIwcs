98369791bf92f5e5e77a4409f836d955167a152c
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-23 10:03:22 +0200

    Adding step1: promise and step2: response

---
+++ index.js
@@ -0,0 +1,5 @@
+fetch("https://www.wildcodeschool.com/")
+    .then(response => response.text())
+    // .then(response => response.text())
+    // .then(data => console.log(data))
+    // .catch(error => console.error(error));
