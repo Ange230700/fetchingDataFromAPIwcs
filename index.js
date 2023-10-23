9b67b9dd7e97a5a8f2900623e0de2fdf94ddbd5d
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-23 10:08:58 +0200

    Adding step4 and step5: error handle

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 fetch("https://www.wildcodeschool.com/")
     .then(response => response.text())
     .then(data => console.log(data))
-    // .catch(error => console.error(error));
+    .catch(error => console.error(error));
