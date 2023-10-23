9225aba86831d09b159cd3fe175879c7430df878
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-23 16:27:29 +0200

    doing challenge

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-fetch("https://www.wildcodeschool.com/")
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+let pageCounter = 1;
+while (pageCounter <= 53) {
+    fetch(`https://www.balldontlie.io/api/v1/players?page=${pageCounter}&per_page=100`)
+        .then(response => response.json())
+        .then(object => object.data.filter(player => player.team.id === 13 && player.height_feet && player.height_inches).forEach(player => {
+            console.log(player);
+        }))
+        .catch(error => console.error(error))
+    pageCounter++;
+}
