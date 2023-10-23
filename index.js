ef526b823f50fe5e500b1edf6cc4ba75c7731c26
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-23 10:07:05 +0200

    Adding step3: data

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 fetch("https://www.wildcodeschool.com/")
     .then(response => response.text())
-    // .then(response => response.text())
-    // .then(data => console.log(data))
+    .then(data => console.log(data))
     // .catch(error => console.error(error));
